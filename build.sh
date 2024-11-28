d4c3e84a3b0f6068224dafc4484f9ba34b838e76
Gungun974 <xfelix974@gmail.com>
2024-11-28 16:25:01 +0400

    Add ffmpeg patch for hls seek

--- build.sh
+++ build.sh
@@ -34,6 +34,9 @@ cat <<EOF >"$BUILD_SCRIPT"
 
     git clone --filter=blob:none --branch='$GIT_BRANCH' '$FFMPEG_REPO' ffmpeg
     cd ffmpeg
+ 
+    git apply patches/hls-seek/0001-hls-seek-patch-1.patch
+    git apply patches/hls-seek/0002-hls-seek-patch-1.patch
 
     ./configure --prefix=/ffbuild/prefix --pkg-config-flags="--static" \$FFBUILD_TARGET_FLAGS \$FF_CONFIGURE \
         --extra-cflags="\$FF_CFLAGS" --extra-cxxflags="\$FF_CXXFLAGS" --extra-libs="\$FF_LIBS" \
